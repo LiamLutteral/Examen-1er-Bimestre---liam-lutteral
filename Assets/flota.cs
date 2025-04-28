fd2ec82e5cfff857046ed4f3687c37ec917a9615
LiamLutteral <49008662@est.ort.edu.ar>
2025-04-28 09:55:42 -0300

    Update flota.cs
    variables

--- Assets/flota.cs
+++ Assets/flota.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class flota : MonoBehaviour
 {
+    private const float kmPorDiaPorUnidad = 90f;
+    private const float kmPorLitro = 15f;
+    private const float costoPorLitro = 130f;
+    private const float descuentoPorcentaje = 0.20f;
+    private const float umbralDescuentoLitros = 100f;
     // Start is called before the first frame update
     void Start()
     {
